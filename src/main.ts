081889cc196faff9cff7a99af98f13d94369f399
project <1300749625@qq.com>
2021-01-04 11:36:34 +0800

    feat：element的全局引入和按需引入完成

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import { installAllElementPlus, installElementPlus } from './plugins/element'
 import { installAllElementPlus } from './plugins/element'
 
 const app = createApp(App)
 
-installAllElementPlus(app)
+// installElementPlus(app) // 按需引入
+installAllElementPlus(app) // 全局引入
 
 app
   .use(store)
