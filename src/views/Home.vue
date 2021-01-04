d03d10fe6da005f28861e61f4e18632a867a27e2
project <1300749625@qq.com>
2021-01-04 17:45:53 +0800

    feat：router和vuex在vue3中的用法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
+    <h1>home页面</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { defineComponent, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { useStore } from 'vuex'
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld
+  setup(props, context) {
+    const router = useRouter() // 获取路由对象
+    const route = useRoute() // 获取路由参数对象
+    const store = useStore() // 获取vuex对象
+
+    onMounted(() => {
+      console.log(router)
+      console.log(route)
+      console.log(store.state.count)
+    })
+
+    return { router, route, store }
   }
 })
 </script>
