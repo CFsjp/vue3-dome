2be4383e9b64d3911ef1079a3a737f80f78a648b
project <1300749625@qq.com>
2021-01-04 11:28:43 +0800

    feat：全局引入element

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App)
+import { installAllElementPlus } from './plugins/element'
+
+const app = createApp(App)
+
+installAllElementPlus(app)
+
+app
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app')
--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -1,66 +1,10 @@
-/**
- * 按需加载 element-ui 组件
- * @description
- * [参考官网 - 按需加载](https://element.eleme.cn/#/zh-CN/component/quickstart#an-xu-yin-ru)
- */
-// import Vue from "vue"
+// 方式一：全局引入
+import ElementPlus from 'element-plus'
+import '../style/element-variables.scss'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
-// // 方式一：按需加载
-// import {
-//   Button,
-//   Loading,
-//   Message,
-//   Dialog,
-//   Upload,
-//   Input,
-//   Autocomplete,
-//   Tooltip,
-//   Select,
-//   Option,
-//   Form,
-//   FormItem,
-//   Card,
-//   Cascader,
-//   Table,
-//   TableColumn,
-//   Tree,
-//   TimePicker
-// } from 'element-ui'
+export const installAllElementPlus = (app: any) => {
+  app.use(ElementPlus, { locale })
+}
 
-// Vue.use(Button)
-// Vue.use(Dialog)
-// Vue.use(Upload)
-// Vue.use(Input)
-// Vue.use(Autocomplete)
-// Vue.use(Tooltip)
-// Vue.use(Select)
-// Vue.use(Option)
-// Vue.use(Form)
-// Vue.use(FormItem)
-// Vue.use(Card)
-// Vue.use(Cascader)
-// Vue.use(Table)
-// Vue.use(TableColumn)
-// Vue.use(Tree)
-// Vue.use(TimePicker)
-
-// Vue.prototype.$loading = Loading.service
-// Vue.prototype.$message = Message
-
-// 方式二：完整加载方式
-// import ElementUI from "element-ui"
-// import "element-ui/lib/theme-chalk/index.css"
-
-// Vue.use(ElementUI)
-
-// const oldArrayFunc = Array.prototype
-// const proto = Object.create(oldArrayFunc)
-
-// const arrFunc = ['push', 'unshift', 'pop']
-
-// arrFunc.forEach(method => {
-//   proto[method] = (method) => {
-//     updated() // 更新视图
-//     oldArrayFunc[method]
-//   }
-// })
+// 方式二：按需引入
